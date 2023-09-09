3ec1ed80aac5d300958aeed0bd35943307de12f6
Prathyusha <prathyusha@simplemail.ai>
2023-09-09 07:50:37 +0530

    commit

--- aws-config.js
+++ aws-config.js
@@ -1,9 +1,7 @@
 const AWS = require('aws-sdk');
 
 AWS.config.update({
-  accessKeyId: 'AKIATBUEM22AALA5L6WA',
-  secretAccessKey: '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',
-  region: 'us-east-1',
+
 });
 
 module.exports = AWS;
